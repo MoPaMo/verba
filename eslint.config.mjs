8eedaadeff8a4a77d6aab475a51a44ff01889a14
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2025-01-07 07:58:06 +0100

    Create eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      '@typescript-eslint/no-unused-expressions': 'off',
+    },
+  }),
+]
+
+export default eslintConfig
