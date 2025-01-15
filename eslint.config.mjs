fcb9e23badef5dd2672dd85b7337b4d65380ace9
MoPaMo <mopamo@users.noreply.github.com>
2025-01-15 15:41:07 +0100

    refactor: update ESLint configuration for improved compatibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,19 @@
-import { FlatCompat } from '@eslint/eslintrc'
+import { FlatCompat } from "@eslint/eslintrc";
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
-})
+});
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next'],
+    extends: ["next"],
     rules: {
-      '@typescript-eslint/no-unused-expressions': 'off',
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "react/no-find-dom-node": "off", // Disable the rule
+
     },
   }),
-]
+];
 
-export default eslintConfig
+export default eslintConfig;
