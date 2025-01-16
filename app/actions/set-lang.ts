eb58e4b5113debf5f7f3e7ca75e2bae25a904140
MoPaMo <mopamo@users.noreply.github.com>
2025-01-16 14:30:20 +0100

    feat: add language selection functionality with validation and centralized language data

--- app/actions/set-lang.ts
+++ app/actions/set-lang.ts
@@ -1,14 +1,21 @@
 "use server";
 
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
 import { getSession } from "next-auth/react";
 const prisma = new PrismaClient();
 
+
+import languages from "@/data/languages";
 export async function setLang(lang: string) {
   const session = await getSession();
   if (!session) {
     return { error: "You must be signed in to pick a new language." };
   }
-  return { success: true };
+
+  // check if lang is valid
+    if (!languages.includes(lang)) {
+        return { error: "Invalid language." };
+    }
+    
+
 }
