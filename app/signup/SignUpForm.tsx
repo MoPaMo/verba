55c95659e084f6fde49121561e35b54ff53a631e
MoPaMo <mopamo@users.noreply.github.com>
2025-01-16 10:38:09 +0100

    feat: refactor account creation logic and update import paths

--- app/signup/SignUpForm.tsx
+++ app/signup/SignUpForm.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Icons } from '../signin/icons'
-import { createAccount } from '@/actions/signup'
+import { createAccount } from '@/app/actions/signup'
 import Link from 'next/link'
 
 export function SignUpForm() {
