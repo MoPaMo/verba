175a690a8bbf95eca86e856ffc9ed26f26fa90af
MoPaMo <mopamo@users.noreply.github.com>
2025-01-16 12:03:43 +0100

    use login-03

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,25 +2,26 @@ import { SignInForm } from "./SignInForm";
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 
-export default async function SignInPage() {
+import { GalleryVerticalEnd } from "lucide-react";
+
+import { LoginForm } from "@/app/signin/login-form";
+
+export default async function LoginPage() {
   const session = await getServerSession();
 
   if (session) {
     redirect("/home");
   }
-
   return (
-    <div className="flex min-h-screen items-center justify-center bg-theme-background">
-      <div className="w-full max-w-md space-y-8 rounded-lg bg-theme-card p-6 shadow-md">
-        <div className="text-center">
-          <h2 className="text-3xl font-extrabold text-theme-primary">
-            Sign In
-          </h2>
-          <p className="mt-2 text-sm text-theme-secondary">
-            Enter your credentials to access your account
-          </p>
-        </div>
-        <SignInForm />
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          Acme Inc.
+        </a>
+        <LoginForm />
       </div>
     </div>
   );
