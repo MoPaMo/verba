6d0047a6f14c37fe5c839b7b2871a34ae9ad809a
MoPaMo <mopamo@users.noreply.github.com>
2025-01-16 12:37:40 +0100

    feat: update branding and improve legal links in SignIn and SignUp pages

--- app/signup/SignUpForm.tsx
+++ app/signup/SignUpForm.tsx
@@ -178,8 +178,8 @@ export function SignUpForm({
         </CardContent>
       </Card>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
+        By clicking continue, you agree to our <a href="/legal/tos">Terms of Service</a>{" "}
+        and <a href="/legal/privacy">Privacy Policy</a>.
       </div>
     </div>
   );
