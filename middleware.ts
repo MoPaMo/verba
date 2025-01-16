d6abf499d4f2190cf9ed8ff9afa7506cc1ed0235
MoPaMo <mopamo@users.noreply.github.com>
2025-01-16 10:24:10 +0100

    feat: add protected page and middleware for authentication handling

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth({
+  pages: {
+    signIn: "/signin",
+  },
+})
+
+export const config = {
+  matcher: [
+    "/private/:path*",
+    "/settings/:path*",
+    "/home/:path*"
+  ]
+}
