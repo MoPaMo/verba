07f362a99417a4171351db93277163a3becf7992
MoPaMo <mopamo@users.noreply.github.com>
2025-01-27 19:56:59 +0100

    feat: add seed script for populating languages in the database

---
+++ seed/language.ts
@@ -0,0 +1,20 @@
+// seed.ts
+import { PrismaClient } from "@prisma/client";
+import languages from "@/data/languages";
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.language.createMany({
+    data: languages,
+    skipDuplicates: true, // Skip if any language already exists
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
