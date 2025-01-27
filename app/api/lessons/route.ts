881a2cc5f86e1c706a892752cf9eeb6206ae70a8
MoPaMo <mopamo@users.noreply.github.com>
2025-01-27 18:53:58 +0100

    feat: add authentication check to GET lessons endpoint

--- app/api/lessons/route.ts
+++ app/api/lessons/route.ts
@@ -14,5 +14,16 @@ const prisma = new PrismaClient();
 const mockUnits: Unit[] = data;
 
 export async function GET() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return new Response(
+      JSON.stringify({ message: "You must be signed in to see this page." }),
+      {
+        status: 401,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
   return NextResponse.json({ units: mockUnits });
 }
